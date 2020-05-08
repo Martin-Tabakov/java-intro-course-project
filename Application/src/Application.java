c2d3477861521a19bd8db074f9f84e39b3a49dbb
Martin Tabakov <58140535+Martin-Tabakov@users.noreply.github.com>
2020-05-08 18:02:59 +0300

    Add RNG

--- Application/src/Application.java
+++ Application/src/Application.java
@@ -1,4 +1,26 @@
+import java.util.Random;
+
 public class Application {
     public static void main(String[] args) {
+
+    }
+
+    /**
+     * Generates a random number withing a certain range
+     * @param lowerBound smallest number to generate
+     * @param upperBound biggest number to generate
+     * @return Random number
+     */
+    public static int throwDice(int lowerBound,int upperBound){
+        Random random = new Random();
+        int rnd;
+        if(lowerBound<0){
+            rnd = random.nextInt(Math.abs(lowerBound)+Math.abs(upperBound)+1);
+            return rnd- Math.abs(lowerBound);
+        }
+        else{
+            rnd = random.nextInt(upperBound-lowerBound+1);
+            return rnd + lowerBound;
+        }
     }
 }
