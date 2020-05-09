75984ce75eb79370a13cb5bed0582a13e27d9a33
Martin Tabakov <58140535+Martin-Tabakov@users.noreply.github.com>
2020-05-09 17:50:51 +0300

    Add Basic Chance Structure

--- Application/src/Chance.java
+++ Application/src/Chance.java
@@ -1,6 +1,21 @@
 public class Chance {
-    int pos;
-    public void activateChance(int playerId){
-        System.out.println("Life gave a chance to Player "+playerId);
+    public void activateChance(Player player){
+        System.out.println("Life gave a chance to Player "+player.id);
+        getChancePositivity(player);
+    }
+    private void getChancePositivity(Player player){
+        int dice= Application.throwDice(1,10);
+        int dice2= Application.throwDice(1,100);
+        if(dice%2==0) positiveChance(dice2,player);
+        else negativeChance(dice2,player);
+
+    }
+    private void positiveChance(int rng,Player player){
+        player.cash+=100;
+        System.out.println("Chance is positive for player" + player.id);
+    }
+    private void negativeChance(int rng,Player player){
+        player.cash-=100;
+        System.out.println("Chance is negative for player" + player.id);
     }
 }
