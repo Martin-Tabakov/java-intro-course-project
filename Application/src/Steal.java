8005b03e3526e96a3d8587be3d6d3d6f81c59407
Martin Tabakov <58140535+Martin-Tabakov@users.noreply.github.com>
2020-05-09 18:02:30 +0300

    Add Basic Steal Structure

--- Application/src/Steal.java
+++ Application/src/Steal.java
@@ -1,6 +1,14 @@
 public class Steal {
-    int pos;
-    public void steal(int playerId){
-        System.out.println("Looks like we have a robber! He is Player "+playerId);
+    int planId=0;
+    boolean isTaken=false;
+
+    public void steal(Player player){
+        System.out.println("Looks like we have a robber! He is Player "+player.id);
+        if(planId==0 && !player.isPlanActive && !isTaken) assignPlan(player);
+    }
+    private void assignPlan(Player player){
+        isTaken=true;
+        planId= player.planId;
+        player.isPlanActive=true;
     }
 }
